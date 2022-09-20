82efdf4f9d5d322f5c7769d5103700b489dc0978
TyperIgor <igor_purosso@hotmail.com>
2022-09-19 23:09:12 -0300

    update new sdk on host application

---
+++ TwitterAnalysis.Application/TwitterAnalysis.Host/Program.cs
@@ -0,0 +1,28 @@
+using TwitterAnalysis.Application.Extension;
+using TwitterAnalysis.Application.Swagger;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplication();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
